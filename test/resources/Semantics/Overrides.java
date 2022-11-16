dfef0571179845a463b237ecc03c4d5ad9a3bce5
Jeffrey Kaufman <10286898+jeffreykaufman@users.noreply.github.com>
2022-11-16 10:09:36 -0800

    Added override test

---
+++ test/resources/Semantics/Overrides.java
@@ -0,0 +1,41 @@
+class SomeClass {
+    public static void main(String[] args) {
+        System.out.println(0);
+    }
+}
+
+class A {
+    public int foo() {
+        return 0;
+    }
+}
+
+class B extends A {
+    public int foo() {
+        return 1;
+    }
+}
+
+class C extends A {
+    public boolean foo() {
+        return true;
+    }
+}
+
+class D extends A {
+    public int foo(int x) {
+        return 2;
+    }
+}
+
+class E {
+    public int foo(int x) {
+        return 3;
+    }
+}
+
+class F extends E {
+    public int foo(boolean x) {
+        return false;
+    }
+}
